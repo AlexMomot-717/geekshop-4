a9d7b3e9851dccec6fc74d91135aba5dfa11b87c
Alex_Momot <momot-717@yandex.ru>
2021-10-31 21:45:55 +0300

    dz-6

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,6 +131,8 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (
-    BASE_DIR / 'media'
+        BASE_DIR / 'media'
 )
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # путь к медиа по методичке
+
+LOGIN_URL = '/auth/login/'
